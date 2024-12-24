d879fc674a9fda0abc0877318b0eca26756253c8
badmagick329 <uzairfarooqi@gmail.com>
2024-12-24 02:45:09 +0000

    feat: add creating filtered playlist sample

--- SpotifyCli.Console/Program.cs
+++ SpotifyCli.Console/Program.cs
@@ -8,6 +8,7 @@ class Program
     public static async Task Main(string[] args)
     {
         var config = new AppConfig();
-        await Examples.PlaylistPlayExample(config);
+        // await Examples.PlaylistPlayExample(config);
+        await Examples.PlaylistCreateExample(config, 2024);
     }
 }
--- SpotifyCli.Console/TestingStuff/TestingStuff.cs
+++ SpotifyCli.Console/TestingStuff/TestingStuff.cs
@@ -23,7 +23,8 @@ class Examples
         await auth.CreateClient();
         var client = auth.Client;
 
-        FullPlaylist playlist = await SamplePlaylist(client);
+        var allPlaylists = await FetchAllPlaylists(client);
+        var playlist = SamplePlaylist(allPlaylists);
         Debug.Assert(playlist.Id is not null);
 
         try
@@ -63,11 +64,15 @@ class Examples
         }
     }
 
-    private static async Task<FullPlaylist> SamplePlaylist(SpotifyClient client)
+    private static async Task<IList<FullPlaylist>> FetchAllPlaylists(SpotifyClient client)
     {
-        var playlists = await client.PaginateAll(
+        return await client.PaginateAll(
             await client.Playlists.CurrentUsers().ConfigureAwait(false)
         );
+    }
+
+    private static FullPlaylist SamplePlaylist(IList<FullPlaylist> playlists)
+    {
         Console.WriteLine($"Total Playlists in your Account: {playlists.Count}");
         // var playlist = playlists[0];
         FullPlaylist? playlist = null;
@@ -124,5 +129,92 @@ class Examples
     {
         var devices = await client.Player.GetAvailableDevices();
         return devices.Devices.FirstOrDefault(d => d.IsActive);
+    }
+
+    public static async Task PlaylistCreateExample(AppConfig config, int year)
+    {
+        var auth = new SpotifyAuthenticator(
+            config.ClientId,
+            config.CredentialsPath,
+            config.CallbackUrl,
+            config.Port
+        );
+        SampleDeviceId = config.SampleDeviceId;
+        PlaylistName = config.PlaylistName;
+
+        await auth.CreateClient();
+        var client = auth.Client;
+
+        var allPlaylists = await FetchAllPlaylists(client);
+        var playlist = SamplePlaylist(allPlaylists);
+        Debug.Assert(playlist.Id is not null);
+        var newPlaylist = await CreateOrFetchPlaylist(client, allPlaylists, year.ToString());
+        Debug.Assert(newPlaylist.Id is not null);
+
+        try
+        {
+            var tracks = (await FetchPlaylistTracks(client, playlist.Id))
+                .Select(t => t.Track)
+                .OfType<FullTrack>()
+                .ToList();
+            var tracksToAdd = new List<string>();
+            foreach (var track in tracks)
+            {
+                var releaseDate = track.Album.ReleaseDate;
+                if (releaseDate is null)
+                {
+                    continue;
+                }
+                if (int.Parse(releaseDate.Split('-')[0]) == year)
+                {
+                    if (tracksToAdd.Count > 99)
+                    {
+                        await client.Playlists.AddItems(
+                            newPlaylist.Id,
+                            new PlaylistAddItemsRequest(tracksToAdd)
+                        );
+                        Console.WriteLine("Added 100 tracks to playlist");
+                        tracksToAdd.Clear();
+                        Console.WriteLine("Reset tracksToAdd");
+                        await Task.Delay(5000);
+                        tracksToAdd.Add(track.Uri);
+                    }
+                    else
+                    {
+                        tracksToAdd.Add(track.Uri);
+                    }
+                }
+            }
+            Console.WriteLine("Finished loop with tracksToAdd count: " + tracksToAdd.Count);
+            if (tracksToAdd.Count > 0)
+            {
+                await client.Playlists.AddItems(
+                    newPlaylist.Id,
+                    new PlaylistAddItemsRequest(tracksToAdd)
+                );
+            }
+        }
+        catch (APIException ex)
+        {
+            Console.WriteLine($"Error playing track: {ex.Message}");
+        }
+    }
+
+    private static async Task<FullPlaylist> CreateOrFetchPlaylist(
+        SpotifyClient client,
+        IList<FullPlaylist> playlists,
+        string newPlaylist
+    )
+    {
+        foreach (var p in playlists)
+        {
+            if (p.Name == newPlaylist)
+            {
+                return p;
+            }
+        }
+        var playlistCreateRequest = new PlaylistCreateRequest(newPlaylist);
+        var me = await client.UserProfile.Current();
+        return await client.Playlists.Create(me.Id, playlistCreateRequest);
     }
 }
